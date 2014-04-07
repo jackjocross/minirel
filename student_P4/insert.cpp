b7e7feb36009c645ef062f90c5014c739bd444dd
Jack Cross <jjcross@umich.edu>
2014-04-06 20:01:14 -0400

    inserting some thingys

--- student_P4/insert.cpp
+++ student_P4/insert.cpp
@@ -1,6 +1,9 @@
 #include "catalog.h"
 #include "query.h"
 #include "index.h"
+#include <iostream>
+#include <cstring>
+
 
 /*
  * Inserts a record into the specified relation
@@ -15,6 +18,51 @@ Status Updates::Insert(const string& relation,      // Name of the relation
                        const attrInfo attrList[])   // Value of attributes specified in INSERT statement
 {
     /* Your solution goes here */
+    AttrDesc *insertAttr;
+    Record rec;
+    rec.length = 0;
+    int insertCnt = attrCnt;
+    Status retStatus = attrCat->getRelInfo(relation, insertCnt, insertAttr);
+    if (retStatus != OK)
+    {
+    	error.print(retStatus);
+    	return retStatus;
+    }
+
+    for (int i = 0;i < attrCnt;++i)
+	{
+		for (int j = 0;j < attrCnt;++j)
+		{
+			/*
+			if (j == attrCnt)
+			{
+				return
+				CHECK ERRORRRSSSSSSSS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+			}
+			*/
+			string attrOne = insertAttr[i].attrName;
+			string attrTwo = attrList[j].attrName;
+			if (attrOne == attrTwo)
+			{
+				rec.length += attrList[i].attrLen;
+				memcpy(&rec.data + insertAttr[i].attrOffset, attrList[j].attrValue, attrList[j].attrLen);
+				break;
+			}
+		}
+    }
+
+    RID inRid;
+    Status insertStatus = attrCat->insertRecord(rec, inRid);
+
+    for (int k = 0;k < attrCnt;++k)
+    {
+    	if (insertAttr[k].indexed == true)
+    	{
+    		Status indexInsert = attrCat->insertEntry(VALUE, RID);
+    	}
+    }
+
+
 
     return OK;
 }
